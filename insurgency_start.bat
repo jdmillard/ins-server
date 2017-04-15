e684103b8464791cb0507764455df8bf5c36e15c
Jeffrey Millard <jeffrey.d.millard@gmail.com>
2017-04-14 21:27:23 -0600

    initial commit

---
+++ insurgency_start.bat
@@ -0,0 +1,8 @@
+@echo off
+title Insurgency Server
+:start
+start /wait /abovenormal srcds.exe -game insurgency -usercon -port 27015 +hostname "millard_insurgency" +map "buhriz_coop checkpoint" +maxplayers 64
+:: with these bot settings, do not start on market_coop map
+:: i chopped off these arguments fyi
+:: -ip <xxx.xxx.xxx.xxx-internal ip> +sv_lan 0
+goto start
