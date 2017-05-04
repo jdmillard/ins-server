e48e91f3419fc2ab1a3c54e800a1986e8084e60c
Jeffrey Millard <jeffrey.d.millard@gmail.com>
2017-05-03 23:12:40 -0600

    server forces soap.vpk

--- insurgency_start.bat
+++ insurgency_start.bat
@@ -1,7 +1,7 @@
 @echo off
 title Insurgency Server
 :start
-start /wait /abovenormal srcds.exe -game insurgency -usercon -port 27015 +hostname "millard_insurgency" +map "buhriz_coop checkpoint" +maxplayers 64
+start /wait /abovenormal srcds.exe -game insurgency -usercon -port 27015 +hostname "millard_insurgency" +map "buhriz_coop checkpoint" +maxplayers 64 +sv_workshop_enabled 1
 :: with these bot settings, do not start on market_coop map
 :: i chopped off these arguments fyi
 :: -ip <xxx.xxx.xxx.xxx-internal ip> +sv_lan 0
