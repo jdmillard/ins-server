9f7da0db3f7e969ab934e78951ee019eac37ee7a
Jeffrey Millard <jeffrey.d.millard@gmail.com>
2017-05-06 14:45:56 -0600

    automatic server naming

--- insurgency_start.bat
+++ insurgency_start.bat
@@ -7,7 +7,7 @@ set @pc_name=%computername%
 if "%@pc_name%" == "VALENCIA" (set @server_name=insurgency_utah)
 
 :: set server name for Jordan's server
-::if "%@pc_name%" == "J3PC" (set @server_name=insurgency_texas)
+if "%@pc_name%" == "J3PC" (set @server_name=insurgency_texas)
 
 :: set server name for John's server
 ::if "%@pc_name%" == "J4PC" (set @server_name=insurgency_arizona)
