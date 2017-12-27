4b4c99649ac17a124db641359b34c11457ab8eb1
Jeffrey Millard <jeffrey.d.millard@gmail.com>
2017-12-26 18:57:18 -0700

    maps now auto-download; other minor changes

--- insurgency_start.bat
+++ insurgency_start.bat
@@ -4,13 +4,13 @@ title Insurgency Server
 set @pc_name=%computername%
 
 :: set server name for Jeff's server
-if "%@pc_name%" == "VALENCIA" (set @server_name=insurgency_utah)
+if "%@pc_name%" == "VALENCIA" (set @server_name=insurgency_massachusetts)
 
 :: set server name for Jordan's server
 if "%@pc_name%" == "DESKTOP-OJA1CR9" (set @server_name=insurgency_texas)
 
 :: set server name for John's server
-::if "%@pc_name%" == "NAME_HERE" (set @server_name=insurgency_arizona)
+if "%@pc_name%" == "MILLARD" (set @server_name=insurgency_arizona)
 
 IF [%@server_name%]==[] (
   echo no preset server name for this computer, using default.
@@ -22,7 +22,7 @@ echo pc name: %@pc_name%
 echo server name: %@server_name%
 
 :start
-start /wait /abovenormal srcds.exe -game insurgency -usercon -port 27015 +hostname "%@server_name%" +map "buhriz_coop checkpoint" +maxplayers 64 +sv_workshop_enabled 1
+start /wait /abovenormal srcds.exe -game insurgency -usercon -port 27015 +hostname "%@server_name%" +map "badlands_b1 checkpoint" +maxplayers 64 +sv_workshop_enabled 1
 :: with these bot settings, do not start on market_coop map
 :: i chopped off these arguments fyi
 :: -ip <xxx.xxx.xxx.xxx-internal ip> +sv_lan 0
